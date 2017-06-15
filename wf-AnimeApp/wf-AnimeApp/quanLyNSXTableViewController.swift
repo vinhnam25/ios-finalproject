302e68fd54f7e56d6a01d0ca656d53b58caf6796
vinhnam25 <vinhnam25@users.noreply.github.com>
2017-06-15 21:17:52 +0700

    thêm quản lý nsx, cập nhật quản lý thể loại

--- wf-AnimeApp/wf-AnimeApp/quanLyNSXTableViewController.swift
+++ wf-AnimeApp/wf-AnimeApp/quanLyNSXTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  quanLyTheLoaiTableViewController.swift
+//  quanLyNSXTableViewController.swift
 //  wf-AnimeApp
 //
 //  Created by Nam Tran on 6/15/17.
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class quanLyTheLoaiTableViewController: UITableViewController {
+class quanLyNSXTableViewController: UITableViewController {
     var data : [[String : AnyObject]] = [[:]]
     var id : [[Int]] = []
     var ten : [[String]] = []
+    var result : String = "0"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,7 @@ class quanLyTheLoaiTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        f_GetAllTheLoai()
+        f_GetAllNSX()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,30 +32,27 @@ class quanLyTheLoaiTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
+        // #warning Incomplete implementation, return the number of sections
         return 1
-        
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //print(data.count)
+        // #warning Incomplete implementation, return the number of rows
         return data.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //print("hiển thị")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "theLoaiCell", for: indexPath)
-
-        //let item = data[indexPath.row]
-        cell.textLabel?.text = data[indexPath.row]["Ten"] as? String
-        return cell
-    }    
-    
-    func f_GetAllTheLoai() {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nsxCell", for: indexPath)
         
-        let url: String = "http://ioswservice.somee.com/api/theloai/getall"
+        cell.textLabel?.text = data[indexPath.row]["TenNSX"] as? String
+        return cell
+    }
+    
+    func f_GetAllNSX() {
+        
+        let url: String = "http://ioswservice.somee.com/api/nsx/getall"
         
         let urlRequest = URLRequest(url: URL(string: url)!)
         
@@ -70,7 +68,7 @@ class quanLyTheLoaiTableViewController: UITableViewController {
                     OperationQueue.main.addOperation {
                         
                         self.tableView.reloadData()
-                        print("get all the loai")
+                        print("get all nsx")
                     }
                     
                 }
@@ -83,9 +81,18 @@ class quanLyTheLoaiTableViewController: UITableViewController {
         tk.resume()
     }
     
-    @IBAction func unwindToQuanLyTheLoai(segue:UIStoryboardSegue) {
+    @IBAction func unwindToQuanLyNSX(segue:UIStoryboardSegue) {
         
+    }    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
     }
+    */
+
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -99,13 +106,13 @@ class quanLyTheLoaiTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            //tableView.deleteRows(at: [indexPath], with: .fade)
-            //XoaTheLoai()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            XoaNSX(id: indexPath.row)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    
+ 
 
     /*
     // Override to support rearranging the table view.
@@ -131,29 +138,66 @@ class quanLyTheLoaiTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func XoaNSX(id: Int) {
+        let url: String = "http://ioswservice.somee.com/api/nsx/xoa/"
+        var urlRequest = URLRequest(url: URL(string: url)!)
+        
+        let tk = URLSession.shared.dataTask(with: urlRequest)
+        {
+            (data, response, error) in
+            if error != nil {
+                print(error.debugDescription)
+            }
+            else {
+                
+                do {
+                    self.result = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! String
+                    
+                    OperationQueue.main.addOperation {
+                        if self.result == "1"
+                        {
+                            self.f_ShowAlert_OK(title: "Thông báo", mess: "Xoá nhà sản xuất thành công.")
+                        }
+                        else{
+                            self.f_ShowAlert_OK(title: "Thông báo", mess: "Không xoá được nhà sản xuất!")
+                        }
+                        
+                    }
+                    
+                }
+                catch let err as NSError {
+                    print(err)
+                    self.f_ShowAlert_OK(title: "Lỗi", mess: "Lỗi xử lý hoặc kết nối máy chủ !")
+                }
+            }
+        }
+        
+        tk.resume()
+    }
 
 }
 
-class themTheLoaiTableViewController : UITableViewController {
+class themNSXTableViewController : UITableViewController {
     var result : String = "0"
     var index: Int?
     var editedTheLoai: String?
-    @IBOutlet var tenTheLoai: UITextField!
+    @IBOutlet var tenNSX: UITextField!
     
     @IBAction func save(sender: AnyObject) {
-        //luuTheLoai()
+        luuNSX()
         dismiss(animated: true, completion: nil)
     }
     
     func luuNSX(){
         if(true) //!isEdit
         {
-            let url: String = "http://ioswservice.somee.com/api/theloai/add"
+            let url: String = "http://ioswservice.somee.com/api/nsx/add"
             
             var urlRequest = URLRequest(url: URL(string: url)!)
             urlRequest.httpMethod = "POST"
             
-            let postString = "Ten=\(tenTheLoai.text!)"
+            let postString = "Ten=\(tenNSX.text!)"
             
             urlRequest.httpBody = postString.data(using: .utf8)
             
@@ -190,12 +234,12 @@ class themTheLoaiTableViewController : UITableViewController {
             tk.resume()
         }
         else {
-            let url: String = "http://ioswservice.somee.com/api/theloai/CapNhat"
+            let url: String = "http://ioswservice.somee.com/api/nsx/CapNhat"
             
             var urlRequest = URLRequest(url: URL(string: url)!)
             urlRequest.httpMethod = "POST"
             
-            let postString = "ten=\(tenTheLoai.text!)&"
+            let postString = "ten=\(tenNSX.text!)&"
             
             urlRequest.httpBody = postString.data(using: .utf8)
             
